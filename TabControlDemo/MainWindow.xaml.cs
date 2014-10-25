94bf3e7f889fcc8eae76bfc4dd3f355f06767524
Maciej Wojton <maciej.wojton@gmail.com>
2014-10-25 17:01:06 -0400

    initial commit of working solution

---
+++ TabControlDemo/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TabControlDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StyledTabControl wnd = new StyledTabControl();
+            wnd.ShowDialog();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            StockTabControl wnd = new StockTabControl();
+            wnd.ShowDialog();
+        }
+    }
+}
---
+++ TabControlDemo/StyledTabControl.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TabControlDemo
+{
+    /// <summary>
+    /// Interaction logic for StyledTabControl.xaml
+    /// </summary>
+    public partial class StyledTabControl : Window
+    {
+        public StyledTabControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
